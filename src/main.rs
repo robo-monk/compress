2e60f7d72bf1e03be8f5241c21e5e93264c76322
robo-monk <rrobomonk@gmail.com>
2022-09-03 16:06:35 +0300

    optimize decompression a bit

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,6 @@ fn write_bytes_to_file(filename: &str, bytes: &[u8]) -> std::io::Result<()> {
         file.write_all(bytes)?;
     }
 
-
-    // {
-    //     let mut file = File::open(filename)?;
-    //     // read the same file back into a Vec of bytes
-    //     let mut buffer = Vec::<u8>::new();
-    //     file.read_to_end(&mut buffer)?;
-    //     println!("{:?}", buffer);
-    // }
     Ok(())
 }
 
@@ -31,28 +23,12 @@ fn read_bytes_from_file(filename: &str) -> std::io::Result<Vec::<u8>> {
         let mut file = File::open(filename)?;
         // read the same file back into a Vec of bytes
         file.read_to_end(&mut buffer)?;
-        println!("{:?}", buffer);
+        // println!("{:?}", buffer);
     }
 
     Ok(buffer)
 }
 
-
-// fn convert_bit_buffer_to_bytes(buf: Vec::<bool>) -> Vec::<u8> {
-//     //  iter.reduce(|accum, item| {
-//     //     if accum >= item { accum } else { item }
-//     // })
-//     let mut current_byte: u8 = 0;
-//     let mut bytes_buffer: Vec::<u8> = Vec::new();
-
-//     buf.into_iter().reduce(|prev, bit| {
-//         println!("{:?}", prev);
-//         // current_byte.rotate_left
-//         return false
-//     });
-
-//     return bytes_buffer;
-// }
 
 fn main() {
     let file_path = "./test.txt";
@@ -68,12 +44,12 @@ fn main() {
 
     let _node = huffman::decode(serialization.to_owned());
 
-    println!("encoded> {}", serialization);
-    println!("decoded> {}", _node.serialize());
+    // println!("encoded> {}", serialization);
+    // println!("decoded> {}", _node.serialize());
     println!(
         "error in serialization? {}",
         if serialization == _node.serialize() {
-            " no "
+            "no "
         } else {
             "yes "
         }
@@ -92,36 +68,37 @@ fn main() {
     // });
     let mut cache: HashMap<char, Vec<bool>> = HashMap::new();
 
-    let compressed_debug: Vec<String> = input
-        .to_owned()
-        .chars()
-        .map(|c| {
-            // let mut code;
-            if !cache.contains_key(&c) {
-                cache.insert(c, tree.traverse(Some(c.to_string())).unwrap_or(vec![]));
-            }
-            let code = cache.get(&c).unwrap();
+    // let compressed_debug: Vec<String> = input
+    //     .to_owned()
+    //     .chars()
+    //     .map(|c| {
+    //         // let mut code;
+    //         if !cache.contains_key(&c) {
+    //             cache.insert(c, tree.traverse(Some(c.to_string())).unwrap_or(vec![]));
+    //         }
+    //         let code = cache.get(&c).unwrap();
 
-            let serialized_code: Vec<&str> = code
-                .into_iter()
-                .map(|&c| if c { "1" } else { "0" })
-                .collect();
-            // code
-            // .into_iter()
-            // .map(|&c| if c { "1" } else { "0" })
+    //         let serialized_code: Vec<&str> = code
+    //             .into_iter()
+    //             .map(|&c| if c { "1" } else { "0" })
+    //             .collect();
+    //         // code
+    //         // .into_iter()
+    //         // .map(|&c| if c { "1" } else { "0" })
 
-            // let serialized_bytes: Vec<u8> =
-            //                         .collect();
+    //         // let serialized_bytes: Vec<u8> =
+    //         //                         .collect();
 
-            return serialized_code.concat();
-        })
-        .collect();
+    //         return serialized_code.concat();
+    //     })
+    //     .collect();
 
     let mut current_byte_index: usize = 0;
     let mut current_byte: u8 = 0;
-    let mut bytes: Vec<u8> = Vec::new();
 
-    input.to_owned().chars().enumerate().for_each(|(i, c)| {
+    let mut bytes: Vec<u8> = serialization.as_bytes().to_vec();
+
+    input.to_owned().chars().for_each(|c| {
         if !cache.contains_key(&c) {
             cache.insert(c, tree.traverse(Some(c.to_string())).unwrap_or(vec![]));
         }
@@ -145,10 +122,10 @@ fn main() {
     } // add remaining bits if existent
 
     println!("bytes len {}", bytes.len());
-    bytes.to_owned().into_iter().for_each(|c| {
-        println!(">{:#02b}", c);
-        // String::from("a")
-    });
+    // bytes.to_owned().into_iter().for_each(|c| {
+    //     println!(">{:#02b}", c);
+    //     // String::from("a")
+    // });
 
     // let mut file = File::create("test.txt.zzz");
     let filename = "test.txt.zzz";
@@ -156,9 +133,9 @@ fn main() {
     let result = write_bytes_to_file(filename, &bytes);
 
 
-    println!("{}\n{}", serialization, compressed_debug.concat());
+    // println!("{}\n{}", serialization, compressed_debug.concat());
     let original_size = input.len() * 8;
-    let compressed_size = compressed_debug.concat().len() + serialization.len();
+    let compressed_size = bytes.len()*8 + serialization.len();
     println!("original size: {} bits \n", original_size);
     println!("compressed size: {} bits \n", compressed_size);
     let compression_rate: f32 = (100 * compressed_size / original_size) as f32;
@@ -166,13 +143,11 @@ fn main() {
 
 
     let read_bytes = read_bytes_from_file(filename).expect("error reading file");
-    println!("read_bytes: {} \n", read_bytes.len());
+    // println!("read_bytes: {} \n", read_bytes.len());
 
     let mut bits: Vec<bool> = Vec::new();
     read_bytes.into_iter().for_each(|mut byte| {
         byte = byte.reverse_bits();
-
-
         for _ in 0..8 { // change it to get range
             let lsb = byte & 1;
             byte >>= 1;
@@ -187,50 +162,37 @@ fn main() {
         // println!("le bytes {bit} || {bit3}")
     });
 
+    println!("done reading bits");
+
     let mut values: Vec<huffman::Val> = Vec::new();
     let mut current_node = tree.to_owned().root;
 
+    // let mut _current_path: Vec<bool> = Vec::new();
+    // let mut _cache: HashMap<Vec<bool>, huffman::Val> = HashMap::new();
+    // very very slow!
     bits.into_iter().for_each(|bit| {
-        if current_node.to_owned().is_leaf() {
-            let value: huffman::Val = current_node.to_owned().value;
+        let mut node = current_node.to_owned();
+
+        if node.is_leaf() {
+            let value: huffman::Val = node.value;
+
             values.push(value);
             // current_node = tree.root.to_owned();
-            current_node = tree.to_owned().root;
+            node = tree.to_owned().root;
         }
 
         if bit {
-            let right = *current_node.to_owned().right.unwrap();
+            let right = *node.right.unwrap();
             current_node = right;
             // current_node = current_node.right.to.as_ref().unwrap();
         } else {
-            let left = *current_node.to_owned().left.unwrap();
+            let left = *node.left.unwrap();
             current_node = left;
         }
     });
 
+    println!("done traversing tree");
     let actual_values: Vec<String> = values.into_iter().map(|value| value.unwrap()).collect();
     let text = actual_values.join("");
     println!("text is {text}");
-
-    // // let mut code;
-    // if !cache.contains_key(&c) {
-    //     cache.insert(c, tree.traverse(Some(c.to_string())).unwrap_or(vec![]));
-    // }
-    // let code = cache.get(&c).unwrap();
-
-    // let serialized_code: Vec<&str> = code
-    //                         .into_iter()
-    //                         .map(|&c| if c { "1" } else { "0" })
-    //                         .collect();
-    // // code
-    //     // .into_iter()
-    //     // .map(|&c| if c { "1" } else { "0" })
-
-    // // let serialized_bytes: Vec<u8> =
-    // //                         .collect();
-
-    // return serialized_code.concat()
-    // });
-
-    // println!("-> bytes \n\n {} \n\n", serialized_bytes.join(" "));
 }
