0075d6d1872e2014e86cce105c0ab2faa4081e96
robo-monk <rrobomonk@gmail.com>
2022-09-10 15:34:03 +0300

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# COMPRESS THE PRESS
+
+a very mediocre implementation of huffman coding in Rust
+
+### source benchmark.sh
+
+benchmark it to see how much worse it is from gzip in terms of actual compression and efficiency :)
--- benchmark.sh
+++ benchmark.sh
@@ -8,3 +8,12 @@ time ./target/release/compress -i test.big.txt -o test.big.txt.zzz
 echo "\ntiming decompress ->"
 time ./target/release/compress -i test.big.txt.zzz -o test.big.out.txt -d
 
+rm test.big.out.txt.gz
+echo "\ntiming gzip compress ->"
+time gzip test.big.out.txt -k
+
+rm test.big.txt
+echo "\ntiming decompress ->"
+time gzip test.big.txt.gz -k -d
+
+
